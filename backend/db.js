b179f50eb50a46f759baca461e7f613297981974
Viral Raval <viral.raval@somaiya.edu>
2021-12-28 11:06:15 +0530

    Final Commit

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const mongoURI = "mongodb://localhost:27017/inote?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+const mongoURI = "mongodb://localhost:27017/inote?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 
 const connectMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
